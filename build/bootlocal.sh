325c1b416ae894bc4aeb8f1d1286d6a17e83ae49
cglewis <clewis@iqt.org>
2015-05-30 21:40:57 -0700

    backdoor for boot2docker-cli

--- build/bootlocal.sh
+++ build/bootlocal.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-echo $'Match User docker\n\tAllowTCPForwarding no\n\tX11Forwarding no\n\tForceCommand TERM=xterm LANG=C.UTF-8 /usr/local/bin/python2.7 /data/menu_launcher.py' >> /usr/local/etc/ssh/sshd_config
+echo $'Match User docker\n\tAllowTCPForwarding no\n\tX11Forwarding no\n\tForceCommand /data/wrapper.sh' >> /usr/local/etc/ssh/sshd_config
 sleep 10
 /usr/local/etc/init.d/openssh stop
 /usr/local/etc/init.d/openssh start
---
+++ build/wrapper.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [ -n "$SSH_ORIGINAL_COMMAND" ]; then
+    exec /bin/sh -c "$SSH_ORIGINAL_COMMAND"
+else
+    TERM=xterm LANG=C.UTF-8 /usr/local/bin/python2.7 /data/menu_launcher.py
+fi
