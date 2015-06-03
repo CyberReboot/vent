e43253c0527aec95c22ec08a5908db659da7ae71
cglewis <clewis@iqt.org>
2015-06-03 10:32:40 -0700

    restructure

---
+++ management/run_tools.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# loop forever
+# read in configuration of tools/settings
+# spin up containers based on configurations
+# periodically cleanup containers/images
+# log everything to stdout
