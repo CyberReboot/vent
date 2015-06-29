02f0f215b6aea5418209a19215257f978ca22c49
cglewis <clewis@iqt.org>
2015-06-29 16:16:33 -0700

    stub for endpoint webserver

---
+++ collectors/vent-collector/endpoint/endpoint.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import web
+
+urls = (
+    '/', 'index'
+)
+
+class index:
+    def GET(self):
+        return {}
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
+
