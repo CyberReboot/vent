43d4524704b4a768728ecd933335f511d316fdf0
cglewis <clewis@iqt.org>
2015-10-20 14:22:46 -0700

    closes #11

--- wrapper.sh
+++ wrapper.sh
@@ -3,5 +3,6 @@
 if [ -n "$SSH_ORIGINAL_COMMAND" ]; then
     exec /bin/sh -c "$SSH_ORIGINAL_COMMAND"
 else
+    sudo /data/custom
     TERM=xterm LANG=C.UTF-8 /usr/local/bin/python2.7 /data/menu_launcher.py
 fi
