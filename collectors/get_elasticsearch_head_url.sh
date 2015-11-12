a12a016c5d17fe7ecb137c411c6d0d0f2d8b26f6
cglewis <clewis@iqt.org>
2015-11-12 14:06:11 -0800

    allow urls to work even if eth1 isn't an interface

--- collectors/get_elasticsearch_head_url.sh
+++ collectors/get_elasticsearch_head_url.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env sh
-running=$(docker ps --filter="name=collectors-elasticsearch" | wc -l); if [ $running == "2" ]; then echo "http://$(/sbin/ifconfig eth1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}'):$(docker inspect -f '{{range $p, $conf := .NetworkSettings.Ports}}{{(index $conf 0).HostPort}} {{end}}' collectors-elasticsearch | awk '{print $1}')/_plugin/head"; else echo "not running"; fi
+/sbin/ifconfig -a|grep ^eth1 &>/dev/null && interface=eth1
+if [ -z "$interface" ]; then ip=$(/usr/local/sbin/ss | grep ssh | awk '{print $5}' | cut -d ':' -f 1); else ip=$(/sbin/ifconfig eth1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}'); fi
+running=$(docker ps --filter="name=collectors-elasticsearch" | wc -l); if [ $running == "2" ]; then echo "http://$ip:$(docker inspect -f '{{range $p, $conf := .NetworkSettings.Ports}}{{(index $conf 0).HostPort}} {{end}}' collectors-elasticsearch | awk '{print $1}')/_plugin/head"; else echo "not running"; fi
