7af4f96d558331eaa640d30e4d42346ff41d4898
Joe Wonohadidjojo <jwonohadidjojo@iqt.org>
2016-06-29 16:37:29 -0700

    Implement args for get_info.sh. Progress: #89

--- info_tools/get_info.sh
+++ info_tools/get_info.sh
@@ -1,57 +1,149 @@
+name=false
+version=false
+build_date=false
+upt=false
+installed_plugins=false
+num_plugins=false
+num_images=false
+num_run_containers=false
+num_stop_containers=false
+active_ssh=false
+installed_images=false
+installed_containers=false
+
+#parse args to print desired information
+if [ "$#" == 0 ];then
+	name=true
+	version=true
+	build_date=true
+	upt=true
+	installed_plugins=true
+elif [ "$1" == "all" ]; then
+	name=true
+	version=true
+	build_date=true
+	upt=true
+	installed_plugins=true
+	num_plugins=true
+	num_images=true
+	num_run_containers=true
+	num_stop_containers=true
+	active_ssh=true
+	if [ "$#" == 2 ]; then
+		if [ "$2" == "-v" ]; then
+			installed_images=true
+			installed_containers=true
+		fi
+	fi
+elif [ "$1" == "name" ]; then
+	name=true
+elif [ "$1" == "count" ]; then
+	if [ "$#" == 2 ]; then
+		if [ "$2" == "plugins" ]; then
+			num_plugins=true
+		elif [ "$2" == "images" ]; then
+			num_images=true
+		elif [ "$2" == "containers" ]; then
+			num_run_containers=true
+			num_stop_containers=true
+		fi
+	else
+		num_plugins=true
+		num_images=true
+		num_run_containers=true
+		num_stop_containers=true
+	fi
+elif [ "$1" == "ssh" ]; then
+	active_ssh=true
+elif [ "$1" == "installed" ]; then
+	if [ "$#" == 2 ]; then
+		if [ "$2" == "images" ]; then
+			installed_images=true
+		elif [ "$2" == "containers" ]; then
+			installed_containers=true
+		elif [ "$2" == "plugins" ]; then
+			installed_plugins=true
+		fi
+	else
+		installed_plugins=true
+		installed_images=true
+		installed_containers=true
+	fi
+fi
 
 #vent instance name
-docker info | grep "Name: "
-echo;
+if [ "$name" == true ]; then
+	docker info | grep "Name: "
+fi
 
 #vent instance version
-echo -n "Version: ";
-cat /data/VERSION | grep -v built
-echo;
+if [ "$version" = true ]; then
+	echo -n "Version: ";
+	cat /data/VERSION | grep -v built
+fi
 
 #date of vent instance creation
-cat /data/VERSION | grep built
-echo;
+if [ "$build_date" = true ]; then
+	cat /data/VERSION | grep built
+fi
 
 #uptime of current vent instance
-echo -n "Uptime: ";
-uptime | awk "{print \$1}"
-echo;
+if [ "$upt" = true ]; then
+	echo -n "Uptime: ";
+	uptime | awk "{print \$1}"
+	echo;
+fi
 
+#number of installed plugins
+if [ "$num_plugins" = true ]; then 
+	echo -n "Number of Plugins: "
+	docker images | grep / | grep -v core | grep -v collectors | grep -v visualization | wc -l;
+fi
 
-#list of installed plugins
-echo "Installed Plugins: ";
-docker images | grep / | grep -v core | grep -v collectors | grep -v visualization | awk "{print \$1}";
-echo;
 
 #number of images
-echo -n "Number of ";
-docker images | grep -v -c CONTAINERS;
-echo;
+if [ "$num_images" = true ]; then
+	echo -n "Number of Images: ";
+	docker images | grep -v -c CONTAINERS;
+fi
 
 #number of running containers
-echo -n "Number of Containers";
-docker info | grep Running:;
-echo;
+if [ "$num_run_containers" = true ]; then
+	echo -n "Number of Containers";
+	docker info | grep Running:;
+fi
 
 #number of stopped containers
-echo -n "Number of Containers";
-docker info | grep Stopped:;
-echo;
+if [ "$num_stop_containers" = true ]; then
+	echo -n "Number of Containers";
+	docker info | grep Stopped:;
+	echo;
+fi
 
 #list of active SSH sessions into this vent instance
-echo "Active SSH Sessions into this Vent instance: ";
-who;
-echo;
+if [ "$active_ssh" = true ]; then
+	echo "Active SSH Sessions into this Vent instance: ";
+	who;
+	echo;
+fi
 
-#verbose
-if [ "$1" == "-v" ] 
-	then
-		#list installed images
-		echo "Installed Images: ";
-		docker images | grep -v REPOSITORY | awk "{print \$1}";
-		echo;
+#list installed plugins
+if [ "$installed_plugins" = true ]; then
+	echo "Installed Plugins: ";
+	docker images | grep / | grep -v core | grep -v collectors | grep -v visualization | awk "{print \$1}";
+	echo;
+fi
 
-		#list all built containers (running and stopped)
-		echo "Containers: ";
-		docker ps -a --format 'table {{.Names}} \t {{.Status}}';
+#list installed images
+if [ "$installed_images" = true ]; then
+	echo "Installed Images: ";
+	docker images | grep -v REPOSITORY | awk "{print \$1}";
+	echo;
+fi
+
+#list all built containers (running and stopped)
+if [ "$installed_containers" = true ]; then
+	echo "Built Containers: ";
+	docker ps -a --format 'table {{.Names}} \t {{.Status}}';
+	echo;
 fi
