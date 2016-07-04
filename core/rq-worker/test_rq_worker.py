c20a16a6eae2d46f5d77bc98b1441497425fbd45
cglewis <clewis@iqt.org>
2016-07-04 16:18:19 -0700

    add more dependencies

--- core/rq-worker/test_rq_worker.py
+++ core/rq-worker/test_rq_worker.py
@@ -2,7 +2,6 @@ import os
 import pytest
 
 import pcap_drop
-import template_change
 
 def test_settings():
     os.environ['REMOTE_REDIS_HOST'] = "test"
@@ -11,6 +10,3 @@ def test_settings():
 
 def test_pcap_queue():
     pcap_drop.pcap_queue("/dev/null")
-
-def test_template_queue():
-    template_change.template_queue("/dev/null")
