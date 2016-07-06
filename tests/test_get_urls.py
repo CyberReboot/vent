fb27a6c001f77298d0dafb1e4d62c5c8f5aa029e
Chiraag Prafullchandra <cprafullchandra@iqt.org>
2016-07-06 15:06:57 -0700

    Added docstring for test get urls.

--- tests/test_get_urls.py
+++ tests/test_get_urls.py
@@ -3,6 +3,7 @@ import pytest
 from ..service_urls import get_urls
 
 def test_url():
+    """ Tests get_urls """
     get_urls.url("elasticsearch", "head")
     get_urls.url("elasticsearch", "marvel")
     get_urls.url("aaa-rabbitmq", "")
