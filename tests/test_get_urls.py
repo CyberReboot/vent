87a01bc83022fbc5f1584737b10568802188c1a0
Chiraag Prafullchandra <cprafullchandra@iqt.org>
2016-07-06 14:42:31 -0700

    Added code coverage for get_urls

--- tests/test_get_urls.py
+++ tests/test_get_urls.py
@@ -4,3 +4,6 @@ from ..service_urls import get_urls
 
 def test_url():
     get_urls.url("elasticsearch", "head")
+    get_urls.url("elasticsearch", "marvel")
+    get_urls.url("aaa-rabbitmq", "")
+    get_urls.url("rq-dashboard","")
