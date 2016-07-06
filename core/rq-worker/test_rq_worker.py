c629c303536a49605e38849b0debb88a45eb4ba1
Chiraag Prafullchandra <cprafullchandra@iqt.org>
2016-07-06 16:43:31 -0700

    Converted tabs to spaces for safety.

--- core/rq-worker/test_rq_worker.py
+++ core/rq-worker/test_rq_worker.py
@@ -4,20 +4,20 @@ import pytest
 import file_watch
 
 def test_settings():
-	""" Tests settings """
-	os.environ['REMOTE_REDIS_HOST'] = "test"
-	os.environ['REMOTE_REDIS_PORT'] = "test"
-	import settings
+    """ Tests settings """
+    os.environ['REMOTE_REDIS_HOST'] = "test"
+    os.environ['REMOTE_REDIS_PORT'] = "test"
+    import settings
 
 def test_pcap_queue():
-	""" Tests simulation of new pcap """
-	file_watch.pcap_queue("/tmp")
-	file_watch.pcap_queue("/dev/null")
+    """ Tests simulation of new pcap """
+    file_watch.pcap_queue("/tmp")
+    file_watch.pcap_queue("/dev/null")
 
 
 def test_template_queue():
-	""" Tests simulation of new/modified template """
-	file_watch.template_queue("/dev/null")
-	file_watch.template_queue("/core.template")
-	file_watch.template_queue("/collectors.template")
-	file_watch.template_queue("/visualization.template")
+    """ Tests simulation of new/modified template """
+    file_watch.template_queue("/dev/null")
+    file_watch.template_queue("/core.template")
+    file_watch.template_queue("/collectors.template")
+    file_watch.template_queue("/visualization.template")
