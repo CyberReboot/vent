bebf4f1f5be58fa142148f6d04fa2ac2ce91e54f
cglewis <clewis@iqt.org>
2016-07-07 21:49:59 -0700

    get ids of containers for hostname check

--- core/rq-worker/test_rq_worker.py
+++ core/rq-worker/test_rq_worker.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import subprocess
 
 import file_watch
 
@@ -17,13 +18,23 @@ def test_pcap_queue():
 
 def test_template_queue():
     """ Tests simulation of new/modified template """
+    os.environ['HOSTNAME'] = "test"
     os.system('docker run -d alpine:latest /bin/sh -c "echo hello world;"')
-    os.system('docker run --name core -d alpine:latest /bin/sh -c "while true; do echo hello world; sleep 1; done"')
-    os.system('docker run --name active -d alpine:latest /bin/sh -c "while true; do echo hello world; sleep 1; done"')
-    os.system('docker run --name passive -d alpine:latest /bin/sh -c "while true; do echo hello world; sleep 1; done"')
-    os.system('docker run --name visualization -d alpine:latest /bin/sh -c "while true; do echo hello world; sleep 1; done"')
+    os.system('docker run --name core-template-queue1 -d alpine:latest /bin/sh -c "while true; do echo hello world; sleep 1; done"')
+    os.system('docker run --name active-template-queue1 -d alpine:latest /bin/sh -c "while true; do echo hello world; sleep 1; done"')
+    os.system('docker run --name passive-template-queue1 -d alpine:latest /bin/sh -c "while true; do echo hello world; sleep 1; done"')
+    os.system('docker run --name visualization-template-queue1 -d alpine:latest /bin/sh -c "while true; do echo hello world; sleep 1; done"')
     file_watch.template_queue("/dev/null")
     file_watch.template_queue("/modes.template")
     file_watch.template_queue("/core.template")
     file_watch.template_queue("/collectors.template")
+    file_watch.template_queue("/visualization.template")
+
+    os.environ['HOSTNAME'] = subprocess.check_output(['docker', 'run --name core-template-queue2 -d alpine:latest /bin/sh -c "while true; do echo hello world; sleep 1; done"'])
+    file_watch.template_queue("/core.template")
+    os.environ['HOSTNAME'] = subprocess.check_output(['docker', 'run --name active-template-queue2 -d alpine:latest /bin/sh -c "while true; do echo hello world; sleep 1; done"'])
+    file_watch.template_queue("/collectors.template")
+    os.environ['HOSTNAME'] = subprocess.check_output(['docker', 'run --name passive-template-queue2 -d alpine:latest /bin/sh -c "while true; do echo hello world; sleep 1; done"'])
+    file_watch.template_queue("/collectors.template")
+    os.environ['HOSTNAME'] = subprocess.check_output(['docker', 'run --name visualization-template-queue2 -d alpine:latest /bin/sh -c "while true; do echo hello world; sleep 1; done"'])
     file_watch.template_queue("/visualization.template")
