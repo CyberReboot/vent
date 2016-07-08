3f6f909502a26ed3c2fa86284d6b273c3344fd6f
cglewis <clewis@iqt.org>
2016-07-07 21:29:42 -0700

    test running some containers

--- core/rq-worker/test_rq_worker.py
+++ core/rq-worker/test_rq_worker.py
@@ -17,7 +17,13 @@ def test_pcap_queue():
 
 def test_template_queue():
     """ Tests simulation of new/modified template """
+    os.system('docker run -d alpine:latest /bin/sh -c "echo hello world;"')
+    os.system('docker run --name core -d alpine:latest /bin/sh -c "while true; do echo hello world; sleep 1; done"')
+    os.system('docker run --name active -d alpine:latest /bin/sh -c "while true; do echo hello world; sleep 1; done"')
+    os.system('docker run --name passive -d alpine:latest /bin/sh -c "while true; do echo hello world; sleep 1; done"')
+    os.system('docker run --name visualization -d alpine:latest /bin/sh -c "while true; do echo hello world; sleep 1; done"')
     file_watch.template_queue("/dev/null")
+    file_watch.template_queue("/modes.template")
     file_watch.template_queue("/core.template")
     file_watch.template_queue("/collectors.template")
     file_watch.template_queue("/visualization.template")
