4c8234e8de8a48f10d3c06c61da1658a7d49a17f
cglewis <clewis@iqt.org>
2016-07-10 18:21:53 -0700

    more tests

--- tests/test_template_parser.py
+++ tests/test_template_parser.py
@@ -3,29 +3,52 @@ import pytest
 
 from .. import template_parser
 
+"""
 def test_read_template_types():
     """ Testing reading from all templates """
     template_dir = "templates/"
     plugins_dir = "plugins/"
-    template_parser.read_template_types("core", "", template_dir, plugins_dir)
-    template_parser.read_template_types("active", "", template_dir, plugins_dir)
-    template_parser.read_template_types("passive", "", template_dir, plugins_dir)
-    template_parser.read_template_types("visualization", "", template_dir, plugins_dir)
-    template_parser.read_template_types("all", "", template_dir, plugins_dir)
+
+    os.system("touch templates/test.template")
+    os.system('echo "[bar]" >> templates/test.template')
+    os.system('echo "HostConfig = {\"PublishAllPorts\": true}" >> templates/test.template')
+    os.system("mkdir plugins/test")
+    os.system("mkdir plugins/test/bar")
+    os.system("mkdir plugins/test/baz")
+    template_parser.read_template_types("test", "", template_dir, plugins_dir)
+    os.system("cp templates/modes.template modes.backup")
+    os.system('echo "test = all" >> templates/modes.template')
+    template_parser.read_template_types("test", "", template_dir, plugins_dir)
+    os.system("cp modes.backup templates/modes.template")
+    os.system('echo "test = bar,baz" >> templates/modes.template')
+    template_parser.read_template_types("test", "", template_dir, plugins_dir)
+    os.system("cp modes.backup templates/modes.template")
 
     os.system("touch templates/foo.template")
-    os.system('echo "[bar]" >> templates/foo.template"')
-    os.system('echo "HostConfig = {\"PublishAllPorts\": true}" >> templates/foo.template"')
+    os.system('echo "[bar]" >> templates/foo.template')
+    os.system('echo "HostConfig = {\"PublishAllPorts\": true, \"Links\": [\"core-aaa-rabbitmq:rabbitmq\"], \"Binds\":[\"/var/log/syslog-ng:/var/log/syslog-ng\"]}" >> templates/foo.template')
+    os.system('echo "site_path = foo" >> templates/foo.template')
+    os.system('echo "data_path = foo" >> templates/foo.template')
+    os.system('echo "delay = 30" >> templates/foo.template')
+    os.system('echo "" >> templates/foo.template')
+    os.system('echo "[baz]" >> templates/foo.template')
+    os.system('echo "HostConfig = {\"PublishAllPorts\": true}" >> templates/foo.template')
     os.system("mkdir plugins/foo")
     os.system("mkdir plugins/foo/bar")
     os.system("mkdir plugins/foo/baz")
     template_parser.read_template_types("foo", "", template_dir, plugins_dir)
-    os.system("cp templates/modes.template modes.backup")
     os.system('echo "foo = all" >> templates/modes.template')
     template_parser.read_template_types("foo", "", template_dir, plugins_dir)
     os.system("cp modes.backup templates/modes.template")
     os.system('echo "foo = bar,baz" >> templates/modes.template')
     template_parser.read_template_types("foo", "", template_dir, plugins_dir)
+    os.system("cp modes.backup templates/modes.template")
+
+    template_parser.read_template_types("core", "", template_dir, plugins_dir)
+    template_parser.read_template_types("active", "", template_dir, plugins_dir)
+    template_parser.read_template_types("passive", "", template_dir, plugins_dir)
+    template_parser.read_template_types("visualization", "", template_dir, plugins_dir)
+    template_parser.read_template_types("all", "", template_dir, plugins_dir)
 
     # Negative Test Cases
     invalid_dir = "tmp/"
@@ -45,3 +68,4 @@ def test_execute_template():
 def test_main():
     """ Testing main function """
     template_parser.main()
+"""
