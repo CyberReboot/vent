405fb907ffaf73f161df2efd15832e5326be94f1
Chiraag Prafullchandra <cprafullchandra@iqt.org>
2016-07-14 15:42:06 -0700

    Test 7 in parallel.

--- tests/test_menu_launcher.py
+++ tests/test_menu_launcher.py
@@ -338,7 +338,7 @@ def test_get_namespace_menu():
 def test_running_menu():
     """ test running the actual menu """
     cmd_invalid_path = "python2.7 menu_launcher.py "
-    UP_KEY = '\E[A'
+    UP_KEY = '\x1b[A'
     child0 = pexpect.spawn(cmd_invalid_path)
     # expect main menu
     child0.expect('Exit')
@@ -346,7 +346,8 @@ def test_running_menu():
     child0.sendline('1')
     child0.expect('Return to Vent menu')
     # go to main menu
-    child0.sendline(curses.KEY_UP)
+    child0.send(curses.UP_KEY)
+    child0.sendline('')
     child0.expect('Exit')
     # exit
     child0.sendline(curses.KEY_UP)
