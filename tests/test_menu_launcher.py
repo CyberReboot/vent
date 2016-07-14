d554fa758f7eda6e7296349dbeaae53dddd8db82
Chiraag Prafullchandra <cprafullchandra@iqt.org>
2016-07-14 15:50:47 -0700

    Fix 2 for test 6.

--- tests/test_menu_launcher.py
+++ tests/test_menu_launcher.py
@@ -346,11 +346,11 @@ def test_running_menu():
     child0.sendline('1')
     child0.expect('Return to Vent menu')
     # go to main menu
-    child0.send(UP_KEY)
+    child0.send(str(curses.KEY_UP))
     child0.sendline('')
     child0.expect('Exit')
     # exit
-    child0.sendline(curses.KEY_UP)
+    child0.sendline(str(curses.KEY_UP))
     child0.read()
     child0.close()
 
