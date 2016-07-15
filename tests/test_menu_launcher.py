95b5ba84543cf9e013703164e141283b2f89eb1a
cglewis <clewis@iqt.org>
2016-07-14 22:36:45 -0700

    should be 5 templates

--- tests/test_menu_launcher.py
+++ tests/test_menu_launcher.py
@@ -399,7 +399,7 @@ def test_running_menu():
     child.sendline('5')
     child.expect('Return to Mode menu')
     # return to mode
-    child.sendline('3')
+    child.sendline('6')
     child.expect('Return to Vent menu')
     # return to main menu
     child.sendline('6')
