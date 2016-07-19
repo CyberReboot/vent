a4e2d1c24dccf991e67c7088dcd6452cd75b0a31
Chiraag Prafullchandra <cprafullchandra@iqt.org>
2016-07-19 14:51:04 -0700

    test_get_status runs before menu_launcher, adjusting to account for errors menu.

--- tests/test_menu_launcher.py
+++ tests/test_menu_launcher.py
@@ -176,7 +176,7 @@ def test_running_menu():
     child.sendline('4')
     child.expect('Return to Mode menu')
     # return to mode
-    child.sendline('7')
+    child.sendline('8')
     child.expect('Return to Vent menu')
     # go to configure
     child.sendline('5')
