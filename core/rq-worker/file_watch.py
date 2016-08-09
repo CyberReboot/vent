d905b5170d336349bf37a094cb50d04cd7d99916
Joecakes4u <joecakes4u@gmail.com>
2016-08-09 16:32:42 -0700

    get_status trollery

--- core/rq-worker/file_watch.py
+++ core/rq-worker/file_watch.py
@@ -124,7 +124,7 @@ def template_queue(path, base_dir="/data/"):
             except Exception as e:
                 pass
 
-        enabled, disabled = check_output("python2.7 "+base_dir+"info_tools/get_status.py enabled", shell=True)
+        enabled, disabled = check_output("python2.7 "+base_dir+"info_tools/get_status.py -b "+base_dir+" enabled", shell=True)
         disabled = ast.literal_eval(disabled)
 
         # remove disabled running containers
