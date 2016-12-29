f56d1a0ee6c8e29af1d107adc732ec00cc2a1da3
Chiraag Prafullchandra <cprafullchandra@iqt.org>
2016-12-29 10:22:02 -0800

    squashed qc issues; fixed global name reference issue for instantiating Template

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -2,18 +2,17 @@ import pytest
 
 from vent.helpers import templates
 
-class Template_Tester:
+class TemplateTester:
+    """ Tester class for Templates.py """
     section1 = "old"
     section2 = "new"
-    section3 = "small"
-    section4 = "big"
     option1 = "alpha"
     option2 = "beta"
     val1 = "text"
     val2 = "more text"
 
     def __init__(self):
-        self.instance = Template()
+        self.instance = templates.Template()
 
     def sections(self):
         return self.instance.sections()
@@ -48,9 +47,9 @@ class Template_Tester:
     def move_option(self, source, destination, option, overwrite=False):
         return self.instance.move_option(source, destination, option, overwrite)
 
-# Test negatives on blank template
 def test_blank_template():
-    test = Template_Tester()
+    """ Test negatives on blank template """
+    test = TemplateTester()
     assert test.sections() == (True, [])
     assert test.section(test.section1)[0] == False
     assert test.options(test.section1)[0] == False
@@ -67,18 +66,18 @@ def test_blank_template():
     # Should pass
     assert test.add_option(test.section2, test.option1, test.val1) == (True, ['alpha'])
 
-# Test insert section that doesn't exist
 def test_add_section():
-    test = Template_Tester()
+    """ Test insert section that doesn't exist """
+    test = TemplateTester()
     assert test.add_section(test.section1) == (True, ['old'])
     assert test.sections() == (True, ['old'])
     assert test.section(test.section1) == (True, [])
     assert test.options(test.section1) == (True, [])
     assert test.option(test.section1, test.option1)[0] == False
 
-# Test insert section that already exists
 def test_add_section_duplicate():
-    test = Template_Tester()
+    """ Test insert section that already exists """
+    test = TemplateTester()
     assert test.add_section(test.section1) == (True, ['old'])
     assert test.add_section(test.section1)[0] == False
     assert test.sections() == (True, ['old'])
@@ -86,18 +85,18 @@ def test_add_section_duplicate():
     assert test.options(test.section1) == (True, [])
     assert test.option(test.section1, test.option1)[0] == False
 
-# Test insert option into section that doesn't exist
 def test_add_option():
-    test = Template_Tester()
+    """ Test insert option into section that doesn't exist """
+    test = TemplateTester()
     assert test.add_option(test.section1, test.option1) == (True, ['alpha'])
     assert test.sections() == (True, ['old'])
     assert test.section(test.section1) == (True, [('alpha', None)])
     assert test.options(test.section1) == (True, ['alpha'])
     assert test.option(test.section1, test.option1) == (True, None)
 
-# Test insert option into section that already exists
 def test_add_option_duplicate_section():
-    test = Template_Tester()
+    """ Test insert option into section that already exists """
+    test = TemplateTester()
     assert test.add_option(test.section1, test.option1) == (True, ['alpha'])
     assert test.add_option(test.section1, test.option2)[0] == False
     assert test.sections() == (True, ['old'])
@@ -105,18 +104,18 @@ def test_add_option_duplicate_section():
     assert test.options(test.section1) == (True, ['alpha'])
     assert test.option(test.section1, test.option1) == (True, None)
 
-# Test insert option with value into section that doesn't exist
 def test_add_option_value_no_section():
-    test = Template_Tester()
+    """ Test insert option with value into section that doesn't exist """
+    test = TemplateTester()
     assert test.add_option(test.section1, test.option1, test.val1) == (True, ['alpha'])
     assert test.sections() == (True, ['old'])
     assert test.section(test.section1) == (True, [('alpha', 'text')])
     assert test.options(test.section1) == (True, ['alpha'])
     assert test.option(test.section1, test.option1) == (True, 'text')
 
-# Test insert option with value into section that exists
 def test_add_option_value():
-    test = Template_Tester()
+    """ Test insert option with value into section that exists """
+    test = TemplateTester()
     assert test.add_section(test.section1) == (True, ['old'])
     assert test.add_option(test.section1, test.option1, test.val1)[0] == False
     assert test.sections() == (True, ['old'])
@@ -124,9 +123,9 @@ def test_add_option_value():
     assert test.options(test.section1) == (True, [])
     assert test.option(test.section1, test.option1)[0] == False
 
-# Test insert option with different value into section that exists
 def test_add_option_value_duplicate_section():
-    test = Template_Tester()
+    """ Test insert option with different value into section that exists """
+    test = TemplateTester()
     assert test.add_option(test.section1, test.option1, test.val1) == (True, ['alpha'])
     assert test.add_option(test.section1, test.option1, test.val2)[0] == False
     assert test.sections() == (True, ['old'])
@@ -134,9 +133,9 @@ def test_add_option_value_duplicate_section():
     assert test.options(test.section1) == (True, ['alpha'])
     assert test.option(test.section1, test.option1) == (True, 'text')
 
-# Test delete option that exists
 def test_del_option():
-    test = Template_Tester()
+    """ Test delete option that exists """
+    test = TemplateTester()
     assert test.add_option(test.section1, test.option1, test.val1) == (True, ['alpha'])
     assert test.del_option(test.section1, test.option1) == (True, [])
     assert test.sections() == (True, ['old'])
@@ -144,9 +143,9 @@ def test_del_option():
     assert test.options(test.section1) == (True, [])
     assert test.option(test.section1, test.option1)[0] == False
 
-# Test delete option that doesn't exist
 def test_del_option_negative():
-    test = Template_Tester()
+    """ Test delete option that doesn't exist """
+    test = TemplateTester()
     assert test.add_option(test.section1, test.option1, test.val1) == (True, ['alpha'])
     assert test.del_option(test.section1, test.option2)[0] == False
     assert test.sections() == (True, ['old'])
@@ -155,18 +154,18 @@ def test_del_option_negative():
     assert test.option(test.section1, test.option1) == (True, 'text')
     assert test.option(test.section1, test.option2)[0] == False
 
-# Test delete option from section that doesn't exist
 def test_del_option_no_section():
-    test = Template_Tester()
+    """ Test delete option from section that doesn't exist """
+    test = TemplateTester()
     assert test.del_option(test.section1, test.option1)[0] == False
     assert test.sections() == (True, [])
     assert test.section(test.section1)[0] == False
     assert test.options(test.section1)[0] == False
     assert test.option(test.section1, test.option1)[0] == False
 
-# Test move option from section to empty section
 def test_move_option():
-    test = Template_Tester()
+    """ Test move option from section to empty section """
+    test = TemplateTester()
     assert test.add_option(test.section1, test.option1, test.val1) == (True, ['alpha'])
     assert test.add_section(test.section2) == (True, ['old', 'new'])
     assert test.move_option(test.section1, test.section2, test.option1) == (True, ['alpha'])
@@ -178,9 +177,9 @@ def test_move_option():
     assert test.option(test.section1, test.option1)[0] == False
     assert test.option(test.section2, test.option1) == (True, 'text')
 
-# Test move duplicate option
 def test_move_option_duplicate():
-    test = Template_Tester()
+    """ Test move duplicate option """
+    test = TemplateTester()
     assert test.add_option(test.section1, test.option1, test.val1) == (True, ['alpha'])
     assert test.add_option(test.section2, test.option1, test.val2) == (True, ['alpha'])
     assert test.move_option(test.section1, test.section2, test.option1)[0] == False
@@ -192,9 +191,9 @@ def test_move_option_duplicate():
     assert test.option(test.section1, test.option1) == (True, 'text')
     assert test.option(test.section2, test.option1) == (True, 'more text')
 
-# Test move duplicate option with overwrite
 def test_move_option_overwrite():
-    test = Template_Tester()
+    """ Test move duplicate option with overwrite """
+    test = TemplateTester()
     assert test.add_option(test.section1, test.option1, test.val1) == (True, ['alpha'])
     assert test.add_option(test.section2, test.option1, test.val2) == (True, ['alpha'])
     assert test.move_option(test.section1, test.section2, test.option1, overwrite=True) == (True, ['alpha'])
@@ -206,9 +205,9 @@ def test_move_option_overwrite():
     assert test.option(test.section1, test.option1)[0] == False
     assert test.option(test.section2, test.option1) == (True, 'text')
 
-# Test move option that doesn't exist
 def test_move_option_negative():
-    test = Template_Tester()
+    """ Test move option that doesn't exist """
+    test = TemplateTester()
     assert test.add_section(test.section1) == (True, ['old'])
     assert test.add_option(test.section2, test.option1, test.val2) == (True, ['alpha'])
     assert test.move_option(test.section1, test.section2, test.option1)[0] == False
@@ -220,9 +219,9 @@ def test_move_option_negative():
     assert test.option(test.section1, test.option1)[0] == False
     assert test.option(test.section2, test.option1) == (True, 'more text')
 
-# Test move option to section that doesn't exist
 def test_move_option_to_no_section():
-    test = Template_Tester()
+    """ Test move option to section that doesn't exist """
+    test = TemplateTester()
     assert test.add_option(test.section1, test.option1, test.val1) == (True, ['alpha'])
     assert test.move_option(test.section1, test.section2, test.option1)[0] == False
     assert test.sections() == (True, ['old'])
@@ -233,9 +232,9 @@ def test_move_option_to_no_section():
     assert test.option(test.section1, test.option1) == (True, 'text')
     assert test.option(test.section2, test.option1)[0] == False
 
-# Test move option from section that doesn't exist
 def test_move_option_from_no_section():
-    test = Template_Tester()
+    """ Test move option from section that doesn't exist """
+    test = TemplateTester()
     assert test.add_option(test.section2, test.option1, test.val1) == (True, ['alpha'])
     assert test.move_option(test.section1, test.section2, test.option1)[0] == False
     assert test.sections() == (True, ['new'])
@@ -246,9 +245,9 @@ def test_move_option_from_no_section():
     assert test.option(test.section1, test.option1)[0] == False
     assert test.option(test.section2, test.option1) == (True, 'text')
 
-# Test set option into section that exists
 def test_set_option():
-    test = Template_Tester()
+    """ Test set option into section that exists """
+    test = TemplateTester()
     assert test.add_section(test.section1) == (True, ['old'])
     assert test.set_option(test.section1, test.option1, test.val1) == (True, None)
     assert test.sections() == (True, ['old'])
@@ -256,18 +255,18 @@ def test_set_option():
     assert test.options(test.section1) == (True, ['alpha'])
     assert test.option(test.section1, test.option1) == (True, 'text')
 
-# Test set option into section that doesn't exist
 def test_set_option_negative():
-    test = Template_Tester()
+    """ Test set option into section that doesn't exist """
+    test = TemplateTester()
     assert test.set_option(test.section1, test.option1, test.val1)[0] == False
     assert test.sections() == (True, [])
     assert test.section(test.section1)[0] == False
     assert test.options(test.section1)[0] == False
     assert test.option(test.section1, test.option1)[0] == False
 
-# Test set duplicate option
 def test_set_option_duplicate():
-    test = Template_Tester()
+    """ Test set duplicate option """
+    test = TemplateTester()
     assert test.add_option(test.section1, test.option1, test.val1) == (True, ['alpha'])
     assert test.set_option(test.section1, test.option1, test.val2) == (True, None)
     assert test.sections() == (True, ['old'])
@@ -275,9 +274,9 @@ def test_set_option_duplicate():
     assert test.options(test.section1) == (True, ['alpha'])
     assert test.option(test.section1, test.option1) == (True, 'more text')
 
-# Test rename empty section that exists
 def test_rename_section():
-    test = Template_Tester()
+    """ Test rename empty section that exists """
+    test = TemplateTester()
     assert test.add_section(test.section1) == (True, ['old'])
     assert test.rename_section(test.section1, test.section2)
     assert test.sections() == (True, ['new'])
@@ -286,18 +285,18 @@ def test_rename_section():
     assert test.section(test.section2) == (True, [])
     assert test.options(test.section2) == (True, [])
 
-# Test rename empty section to itself
 def test_rename_section_itself():
-    test = Template_Tester()
+    """ Test rename empty section to itself """
+    test = TemplateTester()
     assert test.add_section(test.section1) == (True, ['old'])
     assert test.rename_section(test.section1, test.section1)[0] == False
     assert test.sections() == (True, ['old'])
     assert test.section(test.section1) == (True, [])
     assert test.options(test.section1) == (True, [])
 
-# Test rename empty section to duplicate section
 def test_rename_section_duplicate():
-    test = Template_Tester()
+    """ Test rename empty section to duplicate section """
+    test = TemplateTester()
     assert test.add_section(test.section1) == (True, ['old'])
     assert test.add_option(test.section2, test.option1, test.val1) == (True, ['alpha'])
     assert test.rename_section(test.section1, test.section2)[0] == False
@@ -308,9 +307,9 @@ def test_rename_section_duplicate():
     assert test.options(test.section2) == (True, ['alpha'])
     assert test.option(test.section2, test.option1) == (True, 'text')
 
-# Test rename nonempty section
 def test_rename_section_values():
-    test = Template_Tester()
+    """ Test rename nonempty section """
+    test = TemplateTester()
     assert test.add_option(test.section1, test.option1, test.val1) == (True, ['alpha'])
     assert test.rename_section(test.section1, test.section2) == (True, ['new'])
     assert test.sections() == (True, ['new'])
@@ -320,19 +319,19 @@ def test_rename_section_values():
     assert test.options(test.section2) == (True, ['alpha'])
     assert test.option(test.section2, test.option1) == (True, 'text')
 
-# Test delete section that exists
 def test_del_section():
-    test = Template_Tester()
+    """ Test delete section that exists """
+    test = TemplateTester()
     assert test.add_section(test.section1) == (True, ['old'])
     assert test.del_section(test.section1) == (True, [])
     assert test.sections() == (True, [])
     assert test.section(test.section1)[0] == False
     assert test.options(test.section1)[0] == False
 
-# Test delete section that doesn't exist
 def test_del_section_negative():
-   test = Template_Tester()
-   assert test.del_section(test.section1)[0] == False
-   assert test.sections() == (True, [])
-   assert test.section(test.section1)[0] == False
-   assert test.options(test.section1)[0] == False
+    """ Test delete section that doesn't exist """
+    test = TemplateTester()
+    assert test.del_section(test.section1)[0] == False
+    assert test.sections() == (True, [])
+    assert test.section(test.section1)[0] == False
+    assert test.options(test.section1)[0] == False
--- vent/helpers/templates.py
+++ vent/helpers/templates.py
@@ -16,41 +16,41 @@ class Template:
         if template:
             self.config.read(template)
 
-    """ Returns a list of sections """
     @ErrorHandler
     def sections(self):
+        """ Returns a list of sections """
         return (True, self.config.sections())
 
-    """ Returns a list of tuples of (option, value) for the section """
     @ErrorHandler
     def section(self, section):
+        """ Returns a list of tuples of (option, value) for the section """
         # check if the named section exists
         if self.config.has_section(section):
             return (True, self.config.items(section))
         return (False, "Section: " + section + " does not exist")
 
-    """ Returns a list of options for a section """
     @ErrorHandler
     def options(self, section):
+        """ Returns a list of options for a section """
         if self.config.has_section(section):
             return (True, self.config.options(section))
         return (False, "Section: " + section + " does not exist")
 
-    """ Returns the value of the option """
     @ErrorHandler
     def option(self, section, option):
+        """ Returns the value of the option """
         if self.config.has_section(section):
             if self.config.has_option(section, option):
                 return (True, self.config.get(section, option))
             return (False, "Option: " + option + " does not exist")
         return (False, "Section: " + section + " does not exist")
 
-    """
-    If section exists, returns log,
-    otherwise adds section and returns list of sections.
-    """
     @ErrorHandler
     def add_section(self, section):
+        """
+        If section exists, returns log,
+        otherwise adds section and returns list of sections.
+        """
         # check if section already exists
         if not self.config.has_section(section):
             self.config.add_section(section)
@@ -58,12 +58,12 @@ class Template:
             return (True, self.config.sections())
         return (False, "Section: " + section + " already exists")
 
-    """
-    Creates an option for a section. If the section does
-    not exist, it will create the section.
-    """
     @ErrorHandler
     def add_option(self, section, option, value=None):
+        """
+        Creates an option for a section. If the section does
+        not exist, it will create the section.
+        """
         # if duplicate section, and return error message
         message = self.add_section(section)
         if not message[0]:
@@ -80,17 +80,17 @@ class Template:
             return (False, "Option: " + option + " already exists in " + section)
         return (False, "Section: " + section + " does not exist. Did you want to force it?")
 
-    """ Deletes a section if it exists """
     @ErrorHandler
     def del_section(self, section):
+        """ Deletes a section if it exists """
         if self.config.has_section(section):
             self.config.remove_section(section)
             return (True, self.config.sections())
         return (False, "Section: " + section + " does not exist")
 
-    """ Deletes an option if the section and option exist """
     @ErrorHandler
     def del_option(self, section, option):
+        """ Deletes an option if the section and option exist """
         if self.config.has_section(section):
             if self.config.has_option(section, option):
                 self.config.remove_option(section, option)
@@ -98,19 +98,19 @@ class Template:
             return (False, "Option: " + option + " does not exist")
         return (False, "Section: " + section + " does not exist")
 
-    """
-    Sets an option to a value in the given section. Option is created if it
-    does not already exist
-    """
     @ErrorHandler
     def set_option(self, section, option, value):
+        """
+        Sets an option to a value in the given section. Option is created if it
+        does not already exist
+        """
         if self.config.has_section(section):
             return (True, self.config.set(section, option, value))
         return (False, "Section: " + section + " does not exist")
 
-    """ Renames a section by transferring all option-value pairs to a new section """
     @ErrorHandler
     def rename_section(self, original, new):
+        """ Renames a section by transferring all option-value pairs to a new section """
         if self.config.has_section(original):
             if not self.config.has_section(new):
                 values = self.section(original)[1]
@@ -122,9 +122,9 @@ class Template:
             return (False, "Section: " + new + " already exists")
         return (False, "Section: " + original + " does not exist")
 
-    """ Moves an option from one section to another """
     @ErrorHandler
     def move_option(self, source, destination, option, overwrite=False):
+        """ Moves an option from one section to another """
         if self.config.has_section(source):
             if self.config.has_section(destination):
                 if self.config.has_option(source, option):
