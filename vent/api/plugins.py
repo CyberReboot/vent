98e91dcaf11cfd737e3f8cd1c2ea0477c7e1dfc7
cglewis <clewis@iqt.org>
2016-12-30 14:57:06 -0800

    stubbing for adding plugins

--- vent/api/plugins.py
+++ vent/api/plugins.py
@@ -1,8 +1,26 @@
+import subprocess
+
+from helpers.paths import PathDirs
+
 class Plugin:
     """ Handle Plugins """
-    def __init__(self):
-        pass
+    def __init__(self, **kargs):
+        self.path_dirs = PathDirs(**kargs)
+
+    def add(self, repo, tools=[], overrides=[], version=None):
+        """ Adds a plugin of tool(s) """
+        status = subprocess.call("cd "+self.path_dirs.plugins_dir+" && git clone "+repo, shell=True)
+        if status == 0:
+            # TODO check tools, overrides, and version
+            # TODO create a manifest file of which tools are enabled/disabled at which versions, and are built or not.
+            pass
+        else:
+            return ('failed', status)
 
     @staticmethod
-    def add():
+    def add_image(image):
+        return
+
+    @ staticmethod
+    def remove():
         return
