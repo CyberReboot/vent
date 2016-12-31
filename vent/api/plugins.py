9292c3791a9208e0ba3e4b2d7ae043b828095a84
cglewis <clewis@iqt.org>
2016-12-30 20:30:12 -0800

    make clone a bit more robust

--- vent/api/plugins.py
+++ vent/api/plugins.py
@@ -1,3 +1,6 @@
+import errno
+import os
+import shlex
 import subprocess
 
 from helpers.paths import PathDirs
@@ -9,7 +12,22 @@ class Plugin:
 
     def add(self, repo, tools=[], overrides=[], version=None):
         """ Adds a plugin of tool(s) """
-        status = subprocess.call("cd "+self.path_dirs.plugins_dir+" && git clone "+repo, shell=True)
+        org = None
+        name = None
+        if repo.endswith(".git"):
+            repo = repo.split(".git")[0]
+        org, name = repo.split("/")[-2:]
+        path = self.path_dirs.plugins_dir + '/' + org + '/' + name
+        try:
+            os.makedirs(path)
+        except OSError as e:
+            if e.errno == errno.EEXIST and os.path.isdir(path):
+                pass
+            else:
+                return ('failed', e)
+
+        os.chdir(path)
+        status = subprocess.call(shlex.split("git clone " + repo + " ."))
         if status == 0:
             # TODO check tools, overrides, and version
             # TODO create a manifest file of which tools are enabled/disabled at which versions, and are built or not.
