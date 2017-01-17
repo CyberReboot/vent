b4aac902eb32e163fa49ce8faae4bbae24acb7c4
cglewis <clewis@iqt.org>
2017-01-17 10:48:43 -0800

    stub out actions

--- vent/api/actions.py
+++ vent/api/actions.py
@@ -6,3 +6,54 @@ class Action:
    @staticmethod
    def start():
        return
+
+   @staticmethod
+   def stop():
+       return
+
+   @staticmethod
+   def clean():
+       return
+
+   @staticmethod
+   def build():
+       return
+
+   @staticmethod
+   def backup():
+       return
+
+   @staticmethod
+   def restore():
+       return
+
+   @staticmethod
+   def show():
+       # repos, core, tools, images, built, running, etc.
+       return
+
+   @staticmethod
+   def configure():
+       # tools, core, etc.
+       return
+
+   @staticmethod
+   def system_info():
+       return
+
+   @staticmethod
+   def system_conf():
+       return
+
+   @staticmethod
+   def system_commands():
+       # restart, shutdown, etc.
+       return
+
+   @staticmethod
+   def logs():
+       return
+
+   @staticmethod
+   def help():
+       return
