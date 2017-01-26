a8a7553d6b1b9ef715d25b6d8d8b2d44fd4d0d5f
cglewis <clewis@iqt.org>
2017-01-26 15:00:17 -0800

    get more output

--- tests/test_api_plugins.py
+++ tests/test_api_plugins.py
@@ -18,6 +18,7 @@ def test_build_tools():
     """ Test the build_tools function """
     instance = Plugin(base_dir='/tmp/', vent_dir='/tmp/', vendor_dir='/tmp/', scripts_dir='/tmp/')
     status = instance.build_tools(0)
+    assert status[1] == None
     assert status[0] == False
     status = instance.build_tools(128)
     assert status[0] == False
