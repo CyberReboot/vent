8588d51311154709728459277ed3977234a3375d
cglewis <clewis@iqt.org>
2017-01-30 15:39:41 -0800

    try and fix manifest

--- tests/test_api_actions.py
+++ tests/test_api_actions.py
@@ -2,7 +2,7 @@ from vent.api.actions import Action
 
 def test_add():
     """ Test the add function """
-    instance = Action(base_dir='/tmp/', vent_dir='/tmp/', vendor_dir='/tmp/', scripts_dir='/tmp/')
+    instance = Action(base_dir='/tmp/', vent_dir='/tmp/', vendor_dir='/tmp/', scripts_dir='/tmp/', meta_dir='/tmp/.vent')
     status = instance.add('bad')
     assert status[0] == False
     status = instance.add('https://github.com/cyberreboot/vent', branch='experimental', build=False)
@@ -14,14 +14,14 @@ def test_remove():
 
 def test_build():
     """ Test the build function """
-    instance = Action(base_dir='/tmp/', vent_dir='/tmp/', vendor_dir='/tmp/', scripts_dir='/tmp/')
+    instance = Action(base_dir='/tmp/', vent_dir='/tmp/', vendor_dir='/tmp/', scripts_dir='/tmp/', meta_dir='/tmp/.vent')
     instance.add('https://github.com/cyberreboot/vent-plugins', branch='experimental', tools=[('kibana','')], build=False)
     status = instance.build()
     assert status[0] == True
 
 def test_start():
     """ Test the start function """
-    instance = Action(base_dir='/tmp/', vent_dir='/tmp/', vendor_dir='/tmp/', scripts_dir='/tmp/')
+    instance = Action(base_dir='/tmp/', vent_dir='/tmp/', vendor_dir='/tmp/', scripts_dir='/tmp/', meta_dir='/tmp/.vent')
     status = instance.add('https://github.com/cyberreboot/vent-plugins', branch='experimental', tools=[('kibana','')])
     assert status[0] == True
     status = instance.start('kibana')
