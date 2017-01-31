a9df45e3910c384e18a4d06b0dc843ee82d31e81
cglewis <clewis@iqt.org>
2017-01-30 22:40:15 -0800

    let's do this right...

--- tests/test_api_actions.py
+++ tests/test_api_actions.py
@@ -26,7 +26,7 @@ def test_start():
     instance = Action(base_dir=os.getcwd()+'/', vent_dir=os.getcwd()+'/vent/', vendor_dir=os.getcwd()+'/vendor/', scripts_dir=os.getcwd()+'/scripts/', meta_dir=os.getcwd()+'/.vent')
     status = instance.add('https://github.com/cyberreboot/vent-plugins', branch='experimental', tools=[('kibana','')])
     assert status[0] == True
-    status = instance.start('kibana')
+    status = instance.start(name='kibana')
     assert status[0] == True
 
 def test_stop():
