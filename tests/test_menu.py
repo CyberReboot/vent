16697c8bfa1370aac99be2f88f83ab75fdb54d65
cglewis <clewis@iqt.org>
2017-02-01 21:46:19 -0800

    more work on tests

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,10 +1,8 @@
-import npyscreen
-
 from vent.menu import VentForm
 from vent.menu import HelpForm
 from vent.menu import VentApp
 
 def test_onStart():
     """ Test the onStart function """
-    instance = VentApp(npyscreen.NPSAppManaged)
+    instance = VentApp()
     instance.onStart()
