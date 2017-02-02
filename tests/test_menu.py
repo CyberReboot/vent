7791fe3059636cfdda11e16dddcdfe27081cf6d6
cglewis <clewis@iqt.org>
2017-02-01 21:50:37 -0800

    simplify for now

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,8 +1,3 @@
 from vent.menu import VentForm
 from vent.menu import HelpForm
 from vent.menu import VentApp
-
-def test_onStart():
-    """ Test the onStart function """
-    instance = VentApp()
-    instance.onStart()
