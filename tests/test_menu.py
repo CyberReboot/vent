2ae30316de2efa649583944b2bc3a65aa07d4a01
cglewis <clewis@iqt.org>
2017-02-01 21:32:04 -0800

    add some coverage tests

---
+++ tests/test_menu.py
@@ -0,0 +1,10 @@
+import npyscreen
+
+from vent.menu import VentForm
+from vent.menu import HelpForm
+from vent.menu import VentApp
+
+def test_onStart():
+    """ Test the onStart function """
+    instance = VentApp(npyscreen..NPSAppManaged)
+    instance.onStart()
