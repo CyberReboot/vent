5d3c774f2b2098aeb80515547158ba12fcfe755a
cglewis <clewis@iqt.org>
2017-04-23 20:57:24 -0700

    test menu

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,6 +1,12 @@
-try:
-    from vent.menu import HelpForm
-    from vent.menu import VentApp
-    from vent.menu import VentForm
-except Exception as e:
-    pass
+import curses
+import npyscreen
+
+from vent.menu import HelpForm
+from vent.menu import VentApp
+from vent.menu import VentForm
+
+npyscreen.TEST_SETTINGS['TEST_INPUT'] = ['^X']
+npyscreen.TEST_SETTINGS['CONTINUE_AFTER_TEST_INPUT'] = False
+
+app = VentApp()
+app.run(fork=False)
