44c6757ef6ece9da05d552c675dd8d9f5d6b3156
cglewis <clewis@iqt.org>
2017-04-23 21:18:55 -0700

    simplify

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -2,9 +2,6 @@ from vent.menu import HelpForm
 from vent.menu import VentApp
 from vent.menu import VentForm
 
-app = VentApp()
-app.onStart()
-app.change_form("HELP")
-
 form = HelpForm()
+app = VentApp()
 form = VentForm()
