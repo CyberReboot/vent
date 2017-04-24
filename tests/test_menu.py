c7d3429d0835afa0880b06d276af768ee3b2ea67
cglewis <clewis@iqt.org>
2017-04-23 21:23:01 -0700

    more attempts

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,7 +1,15 @@
+import curses
+import npyscreen
+
 from vent.menu import HelpForm
 from vent.menu import VentApp
 from vent.menu import VentForm
 
-form = HelpForm()
-app = VentApp()
-form = VentForm()
+npyscreen.TEST_SETTINGS['TEST_INPUT'] = ['^X', '^T', '^C']
+npyscreen.TEST_SETTINGS['CONTINUE_AFTER_TEST_INPUT'] = False
+
+A = VentApp()
+try:
+    A.run(fork=False)
+except KeyboardInterrupt as e:
+    pass
