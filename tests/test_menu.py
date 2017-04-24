00570e540c2314d4d3498f33d7bdf6733dd72149
cglewis <clewis@iqt.org>
2017-04-23 21:30:50 -0700

    catch all

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -5,11 +5,11 @@ from vent.menu import HelpForm
 from vent.menu import VentApp
 from vent.menu import VentForm
 
-npyscreen.TEST_SETTINGS['TEST_INPUT'] = ['^X', '^T', '^C']
+npyscreen.TEST_SETTINGS['TEST_INPUT'] = ['^X', '^T']
 npyscreen.TEST_SETTINGS['CONTINUE_AFTER_TEST_INPUT'] = True
 
 A = VentApp()
 try:
     A.run(fork=False)
-except KeyboardInterrupt as e:
+except Exception as e:
     pass
