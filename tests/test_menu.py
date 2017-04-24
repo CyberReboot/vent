84fe99c58a5f645894cc1dc9e591a5d5aa4626b3
cglewis <clewis@iqt.org>
2017-04-23 21:15:39 -0700

    try tests differently

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,16 +1,10 @@
-import curses
-import npyscreen
-
 from vent.menu import HelpForm
 from vent.menu import VentApp
 from vent.menu import VentForm
 
-npyscreen.TEST_SETTINGS['TEST_INPUT'] = ['^X', '^T', '^C']
-npyscreen.TEST_SETTINGS['CONTINUE_AFTER_TEST_INPUT'] = True
-
 app = VentApp()
+app.onStart()
+app.change_form("HELP")
 
-try:
-    app.run(fork=True)
-except Exception as e:
-    pass
+form = HelpForm()
+form = VentForm()
