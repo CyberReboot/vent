c857f42d827467cef29878459f42fe0b2d6f5e73
cglewis <clewis@iqt.org>
2017-04-30 22:10:55 -0700

    correct path for file-drop tool

--- tests/test_api_actions.py
+++ tests/test_api_actions.py
@@ -32,7 +32,7 @@ def test_start():
     assert status[0] == True
     status = instance.start(name='kibana', branch='experimental')
     assert status[0] == True
-    status = instance.add('https://github.com/cyberreboot/vent', branch='experimental', tools=[('file-drop','')])
+    status = instance.add('https://github.com/cyberreboot/vent', branch='experimental', tools=[('vent/core/file-drop','')])
     assert status[0] == True
     status = instance.start(groups='core', branch='experimental')
     assert status[0] == True
