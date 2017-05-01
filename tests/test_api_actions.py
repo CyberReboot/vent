4f330b7b3b1775aba1354bc5e589525bd91fb41e
cglewis <clewis@iqt.org>
2017-04-30 22:06:07 -0700

    more test tweaks

--- tests/test_api_actions.py
+++ tests/test_api_actions.py
@@ -4,10 +4,10 @@ from vent.api.actions import Action
 
 def test_add():
     """ Test the add function """
-    instance = Action(base_dir=os.getcwd()+'/', meta_dir=os.getcwd()+'/.vent')
+    instance = Action()
     status = instance.add('bad')
     assert status[0] == False
-    status = instance.add('https://github.com/cyberreboot/vent', branch='experimental', build=False)
+    status = instance.add('https://github.com/cyberreboot/vent', build=False)
     assert status[0] == True
 
 def test_remove():
@@ -20,19 +20,19 @@ def test_remove():
 
 def test_build():
     """ Test the build function """
-    instance = Action(base_dir=os.getcwd()+'/', meta_dir=os.getcwd()+'/.vent')
+    instance = Action()
     status = instance.add('https://github.com/cyberreboot/vent-plugins', branch='experimental', tools=[('kibana','')], build=False)
     status = instance.build(branch='experimental')
     assert status[0] == True
 
 def test_start():
     """ Test the start function """
-    instance = Action(base_dir=os.getcwd()+'/', meta_dir=os.getcwd()+'/.vent')
+    instance = Action()
     status = instance.add('https://github.com/cyberreboot/vent-plugins', branch='experimental', tools=[('kibana','')], groups='foo')
     assert status[0] == True
     status = instance.start(name='kibana', branch='experimental')
     assert status[0] == True
-    status = instance.add('https://github.com/cyberreboot/vent', branch='experimental', groups='core')
+    status = instance.add('https://github.com/cyberreboot/vent', branch='experimental', tools=[('file-drop','')])
     assert status[0] == True
     status = instance.start(groups='core', branch='experimental')
     assert status[0] == True
