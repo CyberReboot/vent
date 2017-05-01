81e9eebdd86c633e03dd81e073ac97b11036ce2e
cglewis <clewis@iqt.org>
2017-04-30 22:33:48 -0700

    add dependency tools

--- tests/test_api_actions.py
+++ tests/test_api_actions.py
@@ -21,18 +21,28 @@ def test_remove():
 def test_build():
     """ Test the build function """
     instance = Action()
-    status = instance.add('https://github.com/cyberreboot/vent-plugins', branch='experimental', tools=[('kibana','')], build=False)
+    status = instance.add('https://github.com/cyberreboot/vent-plugins',
+                          branch='experimental',
+                          tools=[('kibana','')],
+                          build=False)
     status = instance.build(branch='experimental')
     assert status[0] == True
 
 def test_start():
     """ Test the start function """
     instance = Action()
-    status = instance.add('https://github.com/cyberreboot/vent-plugins', branch='experimental', tools=[('kibana','')], groups='foo')
+    status = instance.add('https://github.com/cyberreboot/vent-plugins',
+                          branch='experimental',
+                          tools=[('kibana','')],
+                          groups='foo')
     assert status[0] == True
     status = instance.start(name='kibana', branch='experimental')
     assert status[0] == True
-    status = instance.add('https://github.com/cyberreboot/vent', branch='experimental', tools=[('vent/core/file-drop','')])
+    status = instance.add('https://github.com/cyberreboot/vent',
+                          branch='experimental',
+                          tools=[('vent/core/file-drop',''),
+                                 ('vent/core/redis', ''),
+                                 ('vent/core/syslog', '')])
     assert status[0] == True
     status = instance.start(groups='core', branch='experimental')
     assert status[0] == True
