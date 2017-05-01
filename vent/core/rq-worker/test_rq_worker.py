373400070b36dc79a245b5c9c70da76af8d778c3
cglewis <clewis@iqt.org>
2017-05-01 00:05:35 -0700

    use pythonic names

--- vent/core/rq-worker/test_rq_worker.py
+++ vent/core/rq-worker/test_rq_worker.py
@@ -13,9 +13,9 @@ def test_settings():
 
 def test_file_queue():
     """ Tests simulation of new file """
-    pathDirs = PathDirs()
+    path_dirs = PathDirs()
     images = file_watch.file_queue('/tmp/foo')
     assert images[0] == False
-    images = file_watch.file_queue('host_/tmp/foo', template_path=pathDirs.base_dir)
+    images = file_watch.file_queue('host_/tmp/foo', template_path=path_dirs.base_dir)
     assert images[0] == True
     assert type(images[1]) == list
