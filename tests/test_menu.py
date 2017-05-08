2c92dae94cdb0637ac54862f3780182f20e3720d
cglewis <clewis@iqt.org>
2017-05-08 14:21:17 -0700

    add repo_value and docstrings

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -5,6 +5,9 @@ from vent.menu import AddForm
 from vent.menu import HelpForm
 from vent.menu import VentApp
 from vent.menu import VentForm
+from vent.menu import ServicesForm
+from vent.menu import ChooseToolsForm
+from vent.menu import AddOptionsForm
 from vent.menu import repo_tools
 from vent.menu import repo_values
 
@@ -20,11 +23,15 @@ def test_menu():
         pass
 
 def test_repo_tools():
+    """ Test the repo_tools function """
+    repo_value = {'repo':'https://github.com/cyberreboot/vent', 'versions':{'branch':'commit'}}
     a = VentApp()
     tools = repo_tools(a, 'master')
     assert type(tools) == list
 
 def test_repo_values():
+    """ Test the repo_values function """
+    repo_value = {'repo':'https://github.com/cyberreboot/vent', 'versions':{'branch':'commit'}}
     a = VentApp()
     branches, commits = repo_values(a)
     assert type(branches) == list
