051320ca41ef9817407507617b8b9ad6c10b22fc
cglewis <clewis@iqt.org>
2017-05-11 17:14:57 -0700

    cleanup menus

---
+++ vent/menus/services.py
@@ -0,0 +1,40 @@
+import npyscreen
+
+from vent.helpers.meta import Services
+
+class ServicesForm(npyscreen.FormBaseNew):
+    """ Services form for the Vent CLI """
+    triggered = 0
+    services_tft = None
+    def while_waiting(self):
+        """ Update with current services running """
+        # !! TODO this should trigger and update regardless...
+        if not self.triggered:
+            self.triggered = 1
+            services = Services()
+            if services:
+                self.services_tft.hidden = True
+                self.services_tft.display()
+                for service in services:
+                    value = ""
+                    for val in service[1]:
+                        value += val+", "
+                    title_text = self.add(npyscreen.TitleFixedText,
+                                          name=service[0],
+                                          value=value[:-2])
+                    title_text.display()
+
+    def create(self):
+        """ Override method for creating FormBaseNew form """
+        self.add_handlers({"^T": self.change_forms,'^Q': self.quit})
+        self.services_tft = self.add(npyscreen.TitleFixedText, name='No services running.', value="")
+
+    def quit(self, *args, **kwargs):
+        self.parentApp.switchForm(None)
+
+    def change_forms(self, *args, **keywords):
+        """ Toggles back to main """
+        change_to = "MAIN"
+
+        # Tell the VentApp object to change forms.
+        self.parentApp.change_form(change_to)
