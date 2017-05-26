76d7ac1ea370b125e386b64df6d4d21fd11e6a49
Chiraag Prafullchandra <cprafull@ucsd.edu>
2017-05-26 00:46:40 -0700

    stricter adherence to 80 char per line rule

--- vent/menus/help.py
+++ vent/menus/help.py
@@ -88,17 +88,19 @@ class HelpForm(npyscreen.ActionFormWithMenus):
             elif page == 'Plugins':
                 info_str = """
                 Plugins are user created software hosted on GitHub that Vent
-                can install and run. Plugins are developed following a hybrid of requirements
-                specified both by Docker and Vent. Vent uses Docker to run all plugins
-                so all plugins should be designed to run as a system of containers.
-                Knowledge of linking docker containers may be necessary for more
-                complex tasks that require creating multiple containers for your plugin.
-                For Help on building Plugins, check out the Plugin section in our Help Menu."""
+                can install and run. Plugins are developed following a hybrid of
+                requirements specified both by Docker and Vent. Vent uses Docker
+                to run all plugins so all plugins should be designed to run as a
+                system of containers. Knowledge of linking docker containers may
+                be necessary for more complex tasks that require creating multiple
+                containers for your plugin. For Help on building Plugins, check
+                out the Plugin section in our Help Menu."""
             elif page == 'Tools':
                 info_str = """
-                Tools are the individual building blocks of a Plugin. Each tool should
-                follow S.R.P, and over the entirety of the Plugin should be able accomplish any
-                task desired! For Help on building Tools, check out the Plugin section in our Help Menu."""
+                Tools are the individual building blocks of a Plugin. Each tool
+                should follow S.R.P, and over the entirety of the Plugin should
+                be able accomplish any task desired! For Help on building Tools,
+                check out the Plugin section in our Help Menu."""
             elif page == 'Filetypes':
                 info_str = """
                 The filetypes Vent can support are entirely based on the
@@ -107,34 +109,40 @@ class HelpForm(npyscreen.ActionFormWithMenus):
                 out of the box."""
             elif page == 'Core':
                 info_str = """
-                Core Tools are tools that Vent comes with out of the box. For more info on
-                the Core Tools that Vent starts with please check out this link:
+                Core Tools are tools that Vent comes with out of the box. For
+                more info on the Core Tools that Vent starts with check out this link:
                 https://github.com/CyberReboot/vent/blob/master/docs/images/core_plugins.png"""
             elif page == 'Status':
                 info_str = """
                     You'll notice Vent offers several status types amongst tools/plugins.
-                    Built means that each tool has a Docker image successfully built based
-                    off the provided specs for that tool/plugin. Enabled/Disabled correspond
-                    to user defined settings to enable or disable a tool or set of tools (plugin).
-                    Installed means simply that the plugin has been cloned from GitHub and
-                    installed to the Vent filesystem. No Docker image has been created yet.
-                    Running means that a Docker container has successfully been created from
-                    the corresponding Docker image for a specific tool in a Plugin."""
+                    Built means that each tool has a Docker image successfully built
+                    based off the provided specs for that tool/plugin. Enabled/Disabled
+                    correspond to user defined settings to enable or disable a tool or
+                    set of tools (plugin). Installed means simply that the plugin has
+                    been cloned from GitHub and installed to the Vent filesystem.
+                    No Docker image has been created yet. Running means that a Docker
+                    container has successfully been created from the corresponding
+                    Docker image for a specific tool in a Plugin."""
             elif page == 'Plugin Adding':
                 info_str = """
-                    To add a plugin that you've created, simply open up the Menu from the main page
-                    using ^X. After, press "p" to open up the Plugin menu and then "a" to drop down
-                    into our Plugin installation screen. To add a Plugin, we require a valid GitHub
-                    repository. If your repository is private, you will need to enter a username and
-                    password. Once you have finished that, select OK. If we are successfully able to
-                    connect, you should see your repositories branches listed in our Plugin options menu.
-                    From here, press TAB to cycle between the options, and ENTER to select different
-                    branches to install and build from. You can even choose a specific commit if you like!
-                    Once you've selected those tools and selected OK, Vent will notify you about all tools
-                    it has detected. For more information about how Vent detects tools, see our "Building a
-                    Plugin" section. You may select or deselect the tools you wish to install as part of your
-                    Plugin. When you are done, select OK. If everything works you should get a successful
-                    Add. Select OK, to be returned to the main screen!"""
+                    To add a plugin that you've created, simply open up the Menu
+                    from the main page using ^X. After, press "p" to open up the
+                    Plugin menu and then "a" to drop down into our Plugin
+                    installation screen. To add a Plugin, we require a valid
+                    GitHub repository. If your repository is private, you will
+                    need to enter a username and password. Once you have finished
+                    that, select OK. If we are successfully able to connect, you
+                    should see your repositories branches listed in our Plugin
+                    options menu. From here, press TAB to cycle between the
+                    options, and ENTER to select different branches to install and
+                    build from. You can even choose a specific commit if you like!
+                    Once you've selected those tools and selected OK, Vent will
+                    notify you about all tools it has detected. For more information
+                    about how Vent detects tools, see our "Building a Plugin" section.
+                    You may select or deselect the tools you wish to install as part
+                    of your Plugin. When you are done, select OK. If everything
+                    works you should get a successful Add. Select OK, to be returned
+                    to the main screen!"""
             elif page == 'Plugin Building':
                 # !! TODO
                 info_str = """Stay tuned!"""
