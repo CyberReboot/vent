0637e53950c2c14524939ce643dc804862772aba
cglewis <clewis@iqt.org>
2017-06-05 10:11:34 -0700

    fix tests

--- tests/test_api_actions.py
+++ tests/test_api_actions.py
@@ -65,7 +65,8 @@ def test_clean():
 
 def test_update():
     """ Test the update function """
-    Action.update()
+    instance = Action()
+    status = instance.update()
 
 def test_backup():
     """ Test the backup function """
