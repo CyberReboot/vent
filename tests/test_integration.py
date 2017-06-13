020aee4eeb930aa66c8a49b5eee4363475ca53d9
cglewis <clewis@iqt.org>
2017-06-13 13:18:24 -0700

    with forking

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,40 +1,55 @@
+# -*- coding: utf-8 -*-
+
 import npyscreen
 
+from vent.helpers.paths import PathDirs
 from vent.menu import VentApp
 
 npyscreen.TEST_SETTINGS['CONTINUE_AFTER_TEST_INPUT'] = False
 
 def test_integration():
     """ Run integration tests """
-    # close tutorial
-    npyscreen.TEST_SETTINGS['TEST_INPUT'] = ['\n']
-
-    # go to help
-    npyscreen.TEST_SETTINGS['TEST_INPUT'] += ['\014']
-
-    # go through help menus
-    npyscreen.TEST_SETTINGS['TEST_INPUT'] += ['\018', 'b', 'm', '\n', '\n']
-    npyscreen.TEST_SETTINGS['TEST_INPUT'] += ['\018', 'b', 'p', '\n', '\n']
-    npyscreen.TEST_SETTINGS['TEST_INPUT'] += ['\018', 'b', 't', '\n', '\n']
-    npyscreen.TEST_SETTINGS['TEST_INPUT'] += ['\018', 'b', 'f', '\n', '\n']
-    npyscreen.TEST_SETTINGS['TEST_INPUT'] += ['\018', 'b', 'c', '\n', '\n']
-    npyscreen.TEST_SETTINGS['TEST_INPUT'] += ['\018', 'b', 's', '\n', '\n']
-    npyscreen.TEST_SETTINGS['TEST_INPUT'] += ['\018', 'p', 'a', '\n', '\n']
-    npyscreen.TEST_SETTINGS['TEST_INPUT'] += ['\018', 'p', 'b', '\n', '\n']
-
-    # leave help menu
-    npyscreen.TEST_SETTINGS['TEST_INPUT'] += ['\n']
+    CTRL_T = '^T'
+    CTRL_X = '^X'
+    ENTER = '\n'
 
     # run twice, once with tutorial, once without
     for x in xrange(2):
+        if x == 0:
+            # close tutorial
+            npyscreen.TEST_SETTINGS['TEST_INPUT'] = [ENTER]
+
         A = VentApp()
         try:
-            A.run(fork=False)
+            A.run(fork=True)
         except npyscreen.ExhaustedTestInput as e:
             pass
-        else:
-            raise npyscreen.ExhaustedTestInput
 
-    # add a repo, build it, remove it
-    # add a tool in a repo, build it, remove it
-    # add a set of tools at a specific version in a repo, build them, and remove them
+        if x == 0:
+            # initialize tutorial
+            paths = PathDirs()
+            first_time = paths.ensure_file(paths.init_file)
+
+            # go to help
+            npyscreen.TEST_SETTINGS['TEST_INPUT'] = [CTRL_T]
+
+            # go through help menus
+            npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 'b', 'm', ENTER,
+                                                      ENTER]
+            npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 'b', 'p', ENTER,
+                                                      ENTER]
+            npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 'b', 't', ENTER,
+                                                      ENTER]
+            npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 'b', 'f', ENTER,
+                                                      ENTER]
+            npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 'b', 'c', ENTER,
+                                                      ENTER]
+            npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 'b', 's', ENTER,
+                                                      ENTER]
+            npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 'p', 'a', ENTER,
+                                                      ENTER]
+            npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 'p', 'b', ENTER,
+                                                      ENTER]
+
+            # leave help menu
+            npyscreen.TEST_SETTINGS['TEST_INPUT'] += [ENTER]
