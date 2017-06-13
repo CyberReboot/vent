0e33ae0483765d57e20a63e0d6c2529914fa5723
cglewis <clewis@iqt.org>
2017-06-13 15:08:45 -0700

    fix logs; add more menu tests

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -13,7 +13,9 @@ def test_menu():
     CTRL_T = '^T'
     CTRL_X = '^X'
     ENTER = curses.ascii.CR
+    TAB = curses.ascii.TAB
     RIGHT = curses.KEY_RIGHT
+    DOWN = curses.KEY_DOWN
 
     # initialize tutorial
     paths = PathDirs()
@@ -47,9 +49,41 @@ def test_menu():
     # leave help menu, come back and leave again
     npyscreen.TEST_SETTINGS['TEST_INPUT'] += [ENTER, CTRL_T, RIGHT, ENTER]
 
-    # go through the main menus
+    # go through the core tools menus
     npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 'c', 'i', ENTER]
     npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 'c', 'b', RIGHT, ENTER,
+                                              ENTER]
+    npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 'c', 'c', RIGHT, ENTER,
+                                              ENTER]
+    npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 'c', 'v', CTRL_T]
+    npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 'c', 'r', RIGHT, ENTER,
+                                              ENTER]
+    npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 'c', 's', RIGHT, ENTER,
+                                              ENTER]
+    npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 'c', 'p', RIGHT, ENTER,
+                                              ENTER]
+    npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 'c', 'u', RIGHT, ENTER,
+                                              ENTER]
+
+    # go through the plugins menus
+    # TODO
+
+    # go through the logs menus
+    npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 'l', DOWN, CTRL_T]
+
+    # go through the services running menus
+    npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 's', 'c', CTRL_T]
+    npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 's', 'p', CTRL_T]
+
+    # go through the system commands menus
+    npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, DOWN, DOWN, DOWN, DOWN,
+                                              ENTER, 'r', TAB, RIGHT, ENTER,
+                                              ENTER, ENTER]
+    npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, DOWN, DOWN, DOWN, DOWN,
+                                              ENTER, 'u']
+
+    # go through the tutorials menus
+    npyscreen.TEST_SETTINGS['TEST_INPUT'] += [CTRL_X, 't', 's', 's', RIGHT,
                                               ENTER]
 
     A = VentApp()
