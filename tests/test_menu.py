cd02c265870663ccc2395eec305852a74c3a4fc0
cglewis <clewis@iqt.org>
2017-06-14 14:46:08 -0700

    putting reset back

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -101,9 +101,8 @@ def test_menu():
     run_menu([ENTER, CTRL_X, 's', 'p', CTRL_T])
 
     # go through the system commands menus
-    # commenting out for now as this removes the coverage report, it seems
-    #run_menu([ENTER, CTRL_X, DOWN, DOWN, DOWN, DOWN, ENTER, 'r', TAB, RIGHT,
-    #          ENTER, ENTER, ENTER])
+    run_menu([ENTER, CTRL_X, DOWN, DOWN, DOWN, DOWN, ENTER, 'r', TAB, RIGHT,
+              ENTER, ENTER, ENTER])
     run_menu([ENTER, CTRL_X, DOWN, DOWN, DOWN, DOWN, ENTER, 'u'])
 
     # go through the tutorials menus
