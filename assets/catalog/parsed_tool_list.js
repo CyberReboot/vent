1554c39ec08664f11a1da1ebf9ff348db16bd34a
cglewis <clewis@iqt.org>
2017-06-28 11:18:26 -0700

    add init

---
+++ assets/catalog/parsed_tool_list.js
@@ -0,0 +1,10 @@
+myfcn(
+[
+  {
+    "name": "CSV Row Broadcast",
+    "type": "Processor",
+    "description": "Process CSV files.",
+    "url": "https://github.com/CyberReboot/vent-plugins/tree/master/csv_row_broadcast"
+  }
+]
+)
