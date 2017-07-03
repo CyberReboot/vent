55f7f61352266cdc88269d93b22727d5081d81bb
cglewis <clewis@iqt.org>
2017-07-03 11:49:26 -0700

    update tests for refactor

---
+++ tests/test_api_menu_helpers.py
@@ -0,0 +1,10 @@
+from vent.api.menu_helpers import MenuHelper
+
+
+def test_repo_tools():
+    """ Test the repo_tools function """
+    instance = MenuHelper()
+    status = instance.repo_tools('https://github.com/cyberreboot/vent', 'master', 'HEAD')
+    assert isinstance(status, tuple)
+    assert status[0] == True
+    assert isinstance(status[1], list)
---
+++ tests/test_api_plugin_helpers.py
@@ -0,0 +1,27 @@
+from vent.api.plugin_helpers import PluginHelper
+
+
+def test_get_path():
+    """ Test the get_path function """
+    instance = PluginHelper()
+    path = instance.get_path('https://github.com/cyberreboot/vent')
+    assert path.endswith('.internals/plugins/')
+    path = instance.get_path('https://github.com/cyberreboot/vent-plugins')
+    assert '.internals' not in path
+
+def test_apply_path():
+    """ Test the apply_path function """
+    instance = PluginHelper()
+    status = instance.apply_path('https://github.com/cyberreboot/vent')
+    assert isinstance(status, tuple)
+    assert status[0] == True
+    status = instance.apply_path('https://github.com/cyberreboot/vent.git')
+    assert isinstance(status, tuple)
+    assert status[0] == True
+
+def test_checkout():
+    """ Test the checkout function """
+    instance = PluginHelper()
+    status = instance.checkout()
+    assert isinstance(status, tuple)
+    assert status[0] == True
