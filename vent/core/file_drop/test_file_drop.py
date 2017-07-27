aa0589344409c6ad39366438e032311e886ef6ad
cglewis <clewis@iqt.org>
2017-07-26 21:38:15 -0700

    another attempt to fix tests

--- vent/core/file_drop/test_file_drop.py
+++ vent/core/file_drop/test_file_drop.py
@@ -14,16 +14,17 @@ def test_file_drop_GZHandler():
         event_type = None
         src_path = None
         is_directory = None
-        q = Queue(connection=Redis(host='localhost'), default_timeout=86400)
-        r = StrictRedis(host='localhsot', port=6379, db=0)
+        q = None
+        r = None
 
-        def __init__(self, event_type, src_path, is_directory, q, r):
+        def __init__(self, event_type, src_path, is_directory):
             """ initializes necessary variables for the object """
             self.event_type = event_type
             self.src_path = src_path
             self.is_directory = is_directory
-            self.q = q
-            self.r = r
+            self.q = Queue(connection=Redis(host='localhost'),
+                           default_timeout=86400)
+            self.r = StrictRedis(host='localhsot', port=6379, db=0)
 
     b = Event("created", "/dev/null", False)
     a.process(b)
