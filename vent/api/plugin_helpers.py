2d8217fca5521ace177cb45a03a5f4e1cb926dbe
Jeffrey Wang <jeffwang93@gmail.com>
2017-07-28 10:48:19 -0700

    changed 'error' message to info. No actual error

--- vent/api/plugin_helpers.py
+++ vent/api/plugin_helpers.py
@@ -247,12 +247,10 @@ class PluginHelper:
                         options = "".join(cmds)
                     # store options set for docker
                     try:
-                        if options == "#":
-                            tool_d[c_name][option] = options
-                        else:
-                            tool_d[c_name][option] = literal_eval(options)
+                        tool_d[c_name][option] = literal_eval(options)
                     except Exception as e:  # pragma: no cover
-                        self.logger.error("unable to store the options set for docker: " + str(e))
+                        self.logger.info("unable to literal_eval: " +
+                                str(options))
                         tool_d[c_name][option] = options
 
             if 'labels' not in tool_d[c_name]:
