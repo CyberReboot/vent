7c7d10e164fdfe45e389e02eff242c327512778a
Jeffrey Wang <jeffwang93@gmail.com>
2017-07-31 12:39:21 -0700

    code climate spacing. took out commented out code

--- vent/core/network_tap/ncontrol/rest/start.py
+++ vent/core/network_tap/ncontrol/rest/start.py
@@ -17,7 +17,7 @@ class StartR:
         Can also send 'all' as the ID and every network tap container will be
         started.
 
-        Example input: {'id': "12345"}, {'id': ["123", "456"]}, or {'id': "all"}
+        Example input: {'id': "12345"}, {'id': ["123", "456"]}
         """
         web.header('Content-Type', 'application/json')
 
@@ -26,7 +26,7 @@ class StartR:
         payload = {}
         try:
             payload = ast.literal_eval(data)
-        except Exception as e: # pragma: no cover
+        except Exception as e:  # pragma: no cover
             return 'malformed payload : ' + str(e)
 
         # verify payload has a container ID
@@ -37,33 +37,22 @@ class StartR:
         c = None
         try:
             c = docker.from_env()
-        except Exception as e: # pragma: no cover
+        except Exception as e:  # pragma: no cover
             return 'unable to connect to docker because: ' + str(e)
-
-        # start all network tap containers if keyword 'all' is given
-        # TODO: figure out how to implement all without reinventing the wheel
-        # if payload['id'] == 'all':
-        #    try:
-        #        network_containers = ListR.GET()
-        #        for container in network_containers:
-        #            c.containers.get(container['id']).start()
-        #    except Exception as e: # pragma no cover
-        #        return 'unable to start multiple containers because' + \
-        #               str(e)
 
         # if user gives a list of id, start them all
         if type(payload['id']) == list:
             try:
                 for container_id in payload['id']:
                     c.containers.get(container_id).start()
-            except Exception as e: # pragma: no cover
+            except Exception as e:  # pragma: no cover
                 return 'unable to start list of containers because: ' + str(e)
 
         # if user gives just one id, start it
         else:
             try:
                 c.containers.get(payload['id']).start()
-            except Exception as e: # pragma: no cover
+            except Exception as e:  # pragma: no cover
                 return 'unable to start container because: ' + str(e)
 
         return ('container successfully started: ' + str(payload['id']))
--- vent/core/network_tap/ncontrol/rest/stop.py
+++ vent/core/network_tap/ncontrol/rest/stop.py
@@ -17,7 +17,7 @@ class StopR:
         Can also send 'all' as the ID and every network tap container will be
         stopped.
 
-        Example input: {'id': "12345"}, {'id': ["123", "456"], or {'id': "all"}
+        Example input: {'id': "12345"}, {'id': ["123", "456"]
         """
         web.header('Content-Type', 'application/json')
 
@@ -26,7 +26,7 @@ class StopR:
         payload = {}
         try:
             payload = ast.literal_eval(data)
-        except Exception as e: # pragma: no cover
+        except Exception as e:  # pragma: no cover
             return 'malformed payload : ' + str(e)
 
         # verify payload has a container ID
@@ -37,33 +37,22 @@ class StopR:
         c = None
         try:
             c = docker.from_env()
-        except Exception as e: # pragma: no cover
+        except Exception as e:  # pragma: no cover
             return 'unable to connect to docker because: ' + str(e)
-
-        # stop all network tap containers if keyword 'all' is given
-        # TODO: figure out how to implement all without reinventing the wheel
-        # if payload['id'] == 'all':
-        #    try:
-        #        network_containers = ListR.GET()
-        #        for container in network_containers:
-        #           c.containers.get(container['id']).stop()
-        #    except Exception as e: # pragma no cover
-        #        return 'unable to stop multiple containers because: ' + \
-        #               str(e)
 
         # if user gives a list of id, delete them all
         if type(payload['id']) == list:
             try:
                 for container_id in payload['id']:
                     c.containers.get(container_id).stop()
-            except Exception as e: # pragma: no cover
+            except Exception as e:  # pragma: no cover
                 return 'unable to stop list of containers because: ' + str(e)
 
         # if user gives just one id, delete it
         else:
             try:
                 c.containers.get(payload['id']).stop()
-            except Exception as e: # pragma: no cover
+            except Exception as e:  # pragma: no cover
                 return 'unable to stop container because: ' + str(e)
 
         return ('container successfully stopped: ' + str(payload['id']))
