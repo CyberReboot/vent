25206694c3510699e000bb5aa0fee6258e476298
Jeffrey Wang <jeffwang93@gmail.com>
2017-08-14 11:05:35 -0700

    working on adding tutorial forms

--- vent/menus/tutorial_forms.py
+++ vent/menus/tutorial_forms.py
@@ -6,7 +6,35 @@ class TutorialAddingFilesForm(TutorialForm):
     def __init__(self, *args, **keywords):
         """ Initialize adding files tutorial form fields """
         title = "Adding Files"
-        text = """TODO"""
+        text = """
+        In the main Vent menu, there is a field labeled 'File Drop'. The
+        provided directory is where Vent will be watching. Just add the desired
+        file into the directory and Vent will automatically process those files
+        with their respective plugins.
+
+        To change the directory that Vent watches, on the main menu, there's a
+        field labeled 'User Data'. Within that directory, there exists a file
+        named 'vent.cfg'. Within the file, there is a section titled '[main]'
+        with an option 'files'. Just change the value of 'files' to the desired
+        directory.
+
+
+        Example:
+        Let's assume that all core tools have been added, built, and have been
+        started. Let's also assume there's a plugin that deals with
+        *.csv files. If a .csv file is placed into the File Drop directory,
+        Vent will recognize the file extension and run the plugin. This also
+        works for multiple plugins that deal with the same file type. If there
+        were two plugins that dealt with .csv files, Vent would run both on the
+        same .csv file.
+
+        In addition, Vent also supports plugin pipelines.
+        Let's say there are two plugins: Plugin A processes .csv files and
+        Plugin B processes .csv2 files. A .csv file is added to the File Drop
+        directory, so Vent will start Plugin A, which will process the file and
+        output a .csv2 file in the same File Drop directory. This, in turn,
+        will cause Plugin B to start and process the newly created .csv2 file.
+        """
         next_tutorial = "TUTORIALSETTINGUPSERVICES"
         TutorialForm.__init__(self,
                               title,
@@ -93,7 +121,19 @@ class TutorialBuildingCoresForm(TutorialForm):
     def __init__(self, *args, **keywords):
         """ Initialize building cores tutorial form fields """
         title = "Building Cores"
-        text = """TODO"""
+        text = """
+        Building Vent's core tools is a snap.
+
+        At the main menu, press '^X' to bring up the action menu.
+        Highlight core tools and press 'Enter'.
+        Now highlight 'Build core tools' and press 'Enter'.
+        This will bring up a form with a list of core tools that are
+        able to be built. By default, all tools are selected, but it is
+        possible to select which tools to build using the arrow keys and
+        the 'Space' key.
+
+        The build process will take a few minutes so please be patient.
+        """
         next_tutorial = "TUTORIALSTARTINGCORES"
         TutorialForm.__init__(self,
                               title,
@@ -138,7 +178,21 @@ class TutorialStartingCoresForm(TutorialForm):
     def __init__(self, *args, **keywords):
         """ Initialize starting cores tutorial form fields """
         title = "Starting Cores"
-        text = """TODO"""
+        text = """
+        Once a core tool has been added and built, the only thing left is to
+        start it, so it can begin doing work.
+
+        At the main menu, press '^X' to bring up the action menu.
+        Highlight core tools and press 'Enter'.
+        Now highlight 'Start core tools' and press 'Enter'.
+        This will bring up a form with a list of core tools that are
+        able to be started. By default, all tools are selected, but it is
+        possible to select which tools to start using the arrow keys and
+        the 'Space' key.
+
+        Once the desired tools are selected, press OK and Vent will start up the
+        core tools.
+        """
         next_tutorial = "TUTORIALADDINGPLUGINS"
         TutorialForm.__init__(self,
                               title,
@@ -153,7 +207,64 @@ class TutorialTerminologyForm(TutorialForm):
     def __init__(self, *args, **keywords):
         """ Initialize terminology tutorial form fields """
         title = "Vent Terminology"
-        text = """TODO"""
+        text = """
+        Core Tools: Main set of tools that Vent uses to do work. These tools are
+        the backbone of Vent.
+        Plugin Tools: User added tools that process user defined file types.
+        User Data: File directory that has meta data about Vent. The most
+        important files within User Data are:
+            plugin_manifest: meta data about all added tools
+            vent.log: logs concerning how Vent runs. Almost every function
+            within Vent will write to this log.
+            vent.cfg: allows for the customization of certain functionality
+            within vent. File Drop location can be set here. Certain GPU
+            functionality can be set here.
+        File Drop: File directory that Vent watches. Drop files in here so
+        Vent can see and process them.
+        """
+        next_tutorial = "TUTORIALGETTINGSETUP"
+        TutorialForm.__init__(self,
+                              title,
+                              text,
+                              next_tutorial,
+                              *args,
+                              **keywords)
+
+class TutorialTroubleshootingForm(TutorialForm):
+    """ Tutorial troubleshooting form for the Vent CLI """
+    def __init__(self, *args, **keywords):
+        """ Initialize terminology tutorial form fields """
+        title = "Vent Troubleshooting"
+        text = """
+        Something not working as expected within Vent? Not a problem!
+
+        Let's first get some basic possible errors out of the way.
+        1) Is Docker daemon running? Vent uses Docker containers so it is
+        necessary to have the Docker daemon running.
+        2) Is this the lastest version of Vent? Ways to get the latest Vent version:
+            Using pip: 'pip install vent && vent'
+            Using Docker: 'docker pull cyberreboot/vent'
+                          'docker run -it vent_image_id'
+            Using github: 'git clone https://github.com/CyberReboot/vent'
+                          'cd vent && make && vent'
+
+        Still not working? That's fine! Let's get into the nitty gritty and
+        try to figure out what went wrong.
+
+        Firstly, let's see if it's Vent that's causing the problems.
+        Go to the 'User Data' file and open up 'vent.log' with your favorite
+        text editor. Let's search the key term 'False'. Iterate through the
+        search results and look for 'Status of some_function': False'. This
+        tells us that one of Vent's core functions is not performing as
+        expected. Next to it, there will be an error message explaining what
+        went wrong. If it's something with Vent's implementation, please create
+        an issue here: https://github.com/CyberReboot/vent/issues
+
+        If there's no obvious error messages within 'vent.log', let's check any
+        added plugin tools. Run the command 'docker logs plugin_container_id'.
+        This will return all information about the plugin's container and any
+        information regarding the error should be displayed here.
+        """
         next_tutorial = "TUTORIALGETTINGSETUP"
         TutorialForm.__init__(self,
                               title,
