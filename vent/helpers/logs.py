64f7abe1b5042196eff359db6c2e62b29953bc19
Jeff Wang <jeffwang93@gmail.com>
2017-09-28 13:51:08 -0700

    a little touch of formatting

--- vent/helpers/logs.py
+++ vent/helpers/logs.py
@@ -14,7 +14,7 @@ def Logger(name, **kargs):
     handler = logging.handlers.WatchedFileHandler(os.path.join(
         path_dirs.meta_dir, "vent.log"))
     handler.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(name)s:%(lineno)-3d - '
+    formatter = logging.Formatter('%(asctime)s - %(name)s:%(lineno)-4d - '
                                   '%(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     if not len(logger.handlers):
