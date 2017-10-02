129d7ea1768e0e4fa6e13950d38dd175c0618c1f
Arun Pattni <arun.pattni01@gmail.com>
2017-10-02 18:51:02 +0100

    Added Tools List for issue #886

--- assets/catalog/parsed_tool_list.js
+++ assets/catalog/parsed_tool_list.js
@@ -1,10 +1,54 @@
 myfcn(
+
 [
   {
     "name": "CSV Row Broadcast",
     "type": "Processor",
     "description": "Process CSV files.",
     "url": "https://github.com/CyberReboot/vent-plugins/tree/master/csv_row_broadcast"
+  },
+  {
+    "name": "Dshell Netflow Parser",
+    "type": "Pcap",
+    "description": "DShell Feature generation from PCAPs.",
+    "url": "https://github.com/CyberReboot/vent-plugins/tree/master/dshell_netflow_parser"
+  },
+  {
+    "name": "GPU Example",
+    "type": "Example",
+    "description": "Simple GPU Example",
+    "url": "https://github.com/CyberReboot/vent-plugins/blob/master/gpu_example/"
+  },
+  {
+    "name": "Grafana",
+    "type": "Visualization",
+    "description": "Tool for Visualization",
+    "url": "https://github.com/CyberReboot/vent-plugins/blob/master/grafana/"
+  },
+  {
+    "name": "Kibana",
+    "type": "Visualization",
+    "description": "Tool for Visualization",
+    "url": "https://github.com/CyberReboot/vent-plugins/blob/master/kibana/"
+  },
+  {
+    "name": "Pcap Node Splitter",
+    "type": "Pcap",
+    "description": "Packet Capture Splitter",
+    "url": "https://github.com/CyberReboot/vent-plugins/blob/master/pcap_to_node_pcap/"
+  },
+  {
+    "name": "Replay Pcap",
+    "type": "Pcap",
+    "description": "Replays Pcaps",
+    "url": "https://github.com/CyberReboot/vent-plugins/blob/master/replay_pcap/"
+  },
+  {
+    "name": "TCPDump Hex Parser",
+    "type": "Pcap",
+    "description": "TCPDump Feature generation from PCAPs",
+    "url": "https://github.com/CyberReboot/vent-plugins/blob/master/tcpdump_hex_parser/"
   }
 ]
+
 )
