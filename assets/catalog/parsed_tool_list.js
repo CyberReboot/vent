93a9fbc7f0f2515b5498d70a729857c0a999154b
cglewis <clewis@iqt.org>
2017-10-02 18:10:07 -0400

    fix types

--- assets/catalog/parsed_tool_list.js
+++ assets/catalog/parsed_tool_list.js
@@ -3,55 +3,55 @@ myfcn(
 [
   {
     "name": "Elasticsearch",
-    "type": "Core",
+    "type": "Collector",
     "description": "Elasticsearch provides a distributed, multitenant-capable full-text search engine with an HTTP web interface and schema-free JSON documents.",
     "url": "https://github.com/CyberReboot/vent/tree/master/vent/core/elasticsearch"
   },
   {
     "name": "FileDrop",
-    "type": "Core",
+    "type": "Collector",
     "description": "FileDrop watches a directory for new files and sends an event when a new file is added.",
     "url": "https://github.com/CyberReboot/vent/tree/master/vent/core/file_drop"
   },
   {
     "name": "NetworkTap",
-    "type": "Core",
+    "type": "Collector",
     "description": "NetworkTap listens on a given Network Interface Controller(NIC).",
     "url": "https://github.com/CyberReboot/vent/tree/master/vent/core/network_tap"
   },
   {
     "name": "RabbitMQ",
-    "type": "Core",
+    "type": "Processor",
     "description": "RabbitMQ is an open source message broker software (sometimes called message-oriented middleware) that implements the Advanced Message Queuing Protocol (AMQP). ",
     "url": "https://github.com/CyberReboot/vent/tree/master/vent/core/rabbitmq"
   },
   {
     "name": "Redis",
-    "type": "Core",
+    "type": "Collector",
     "description": "Redis is an open-source in-memory database project implementing a networked, in-memory key-value store with optional durability.",
     "url": "https://github.com/CyberReboot/vent/tree/master/vent/core/redis"
   },
   {
     "name": "RMQ-ES-Connector",
-    "type": "Core",
+    "type": "Processor",
     "description": "RMQ-ES-Connector is a connector between RabbitMQ and Elasticsearch.",
     "url": "https://github.com/CyberReboot/vent/tree/master/vent/core/rmq_es_connector"
   },
   {
     "name": "RQ-Dashboard",
-    "type": "Core",
+    "type": "Visualizer",
     "description": "RQ-Dashboard is q-dashboard is a general purpose, lightweight, Flask-based web front-end to monitor your RQ queues, jobs, and workers in realtime.",
     "url": "https://github.com/CyberReboot/vent/tree/master/vent/core/rq_dashboard"
   },
   {
     "name": "RQ-Worker",
-    "type": "Core",
+    "type": "Processor",
     "description": "RQ-Worker is a simple Python library for queueing jobs and processing them in the background with workers.",
     "url": "https://github.com/CyberReboot/vent/tree/master/vent/core/rq_worker"
   },
   {
     "name": "Syslog",
-    "type": "Core",
+    "type": "Processor",
     "description": "Syslog is a way for network devices to send event messages to a logging server – usually known as a Syslog server.",
     "url": "https://github.com/CyberReboot/vent/tree/master/vent/core/syslog"
   },
@@ -63,43 +63,43 @@ myfcn(
   },
   {
     "name": "Dshell Netflow Parser",
-    "type": "Pcap",
+    "type": "Processor",
     "description": "DShell Feature generation from PCAPs.",
     "url": "https://github.com/CyberReboot/vent-plugins/tree/master/dshell_netflow_parser"
   },
   {
     "name": "GPU Example",
-    "type": "Example",
+    "type": "Processor",
     "description": "Simple GPU Example",
     "url": "https://github.com/CyberReboot/vent-plugins/blob/master/gpu_example/"
   },
   {
     "name": "Grafana",
-    "type": "Visualization",
+    "type": "Visualizer",
     "description": "Tool for Visualization",
     "url": "https://github.com/CyberReboot/vent-plugins/blob/master/grafana/"
   },
   {
     "name": "Kibana",
-    "type": "Visualization",
+    "type": "Visualizer",
     "description": "Tool for Visualization",
     "url": "https://github.com/CyberReboot/vent-plugins/blob/master/kibana/"
   },
   {
     "name": "Pcap Node Splitter",
-    "type": "Pcap",
+    "type": "Processor",
     "description": "Packet Capture Splitter",
     "url": "https://github.com/CyberReboot/vent-plugins/blob/master/pcap_to_node_pcap/"
   },
   {
     "name": "Replay Pcap",
-    "type": "Pcap",
+    "type": "Processor",
     "description": "Replays Pcaps",
     "url": "https://github.com/CyberReboot/vent-plugins/blob/master/replay_pcap/"
   },
   {
     "name": "TCPDump Hex Parser",
-    "type": "Pcap",
+    "type": "Processor",
     "description": "TCPDump Feature generation from PCAPs",
     "url": "https://github.com/CyberReboot/vent-plugins/blob/master/tcpdump_hex_parser/"
   }
