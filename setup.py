b0a44480522ef0c1d5369691b54421cbceea647b
cglewis <clewis@iqt.org>
2018-06-29 12:21:08 -0700

    release v0.6.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='vent',
-    version='v0.6.1.dev',
+    version='v0.6.2',
     packages=['vent', 'vent.core', 'vent.core.file_drop',
               'vent.core.rq_worker', 'vent.core.rq_dashboard', 'vent.menus',
               'vent.core.network_tap', 'vent.core.network_tap.ncontrol',
               'vent.core.rmq_es_connector', 'vent.helpers', 'vent.api'],
-    install_requires=['docker>=3.3.0', 'npyscreen', 'pyyaml'],
+    install_requires=['docker>=3.4.0', 'npyscreen>=4.10.5', 'pyyaml>=3.12'],
     scripts=['bin/vent'],
     license='Apache License 2.0',
     author='arpit',
