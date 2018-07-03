88abbc225efaf841272121edd39815ca84967aa4
cglewis <clewis@iqt.org>
2018-07-03 12:36:15 -0700

    fix checkout

--- vent/api/plugins.py
+++ vent/api/plugins.py
@@ -284,7 +284,7 @@ class Plugin:
 
         # check result of clone, ensure successful or that it already exists
         if status:
-            if self.update_repo:
+            if self.update_repo or self.branch != 'master' or self.version != 'HEAD':
                 response = self.p_helper.checkout(branch=self.branch,
                                                   version=self.version)
             else:
