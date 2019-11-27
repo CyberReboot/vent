ba46c26f3fa3bbb936447ddacedf1e86d94073a8
cglewis <clewis@iqt.org>
2019-11-27 09:01:26 -0800

    send message directly from ncapture

---
+++ vent/extras/network_tap/ncapture/send_message.py
@@ -0,0 +1,60 @@
+"""
+Sends message to RabbitMQ to notify capture is complete and written to a file
+
+Created on 27 November 2019
+@author: Charlie Lewis
+"""
+
+import os
+
+import pika
+
+def connect_rabbit(host='messenger', port=5672, queue='task_queue'):
+    params = pika.ConnectionParameters(host=host, port=port)
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel()
+    channel.queue_declare(queue=queue, durable=True)
+    return channel
+
+def send_rabbit_msg(msg, channel, exchange='', routing_key='task_queue'):
+    channel.basic_publish(exchange=exchange,
+                          routing_key=routing_key,
+                          body=json.dumps(msg),
+                          properties=pika.BasicProperties(delivery_mode=2,))
+    print(" [X] %s UTC %r %r" % (str(datetime.datetime.utcnow()),
+                                 str(msg['id']), str(msg['file_path'])))
+
+def get_version():
+    version = ''
+    with open('VERSION', 'r') as f:
+        for line in f:
+            version = line.strip()
+    return version
+
+def get_path(paths):
+    path = None
+    try:
+        path = paths[0]
+    except Exception as e:
+        print("No path provided: {0}, quitting".format(str(e)))
+    return path
+
+def parse_args(parser):
+    parser.add_argument('paths', nargs='*')
+    parsed_args = parser.parse_args()
+    return parsed_args
+
+if __name__ == '__main__':  # pragma: no cover
+    parsed_args = parse_args(argparse.ArgumentParser())
+    path = get_path(parsed_args.paths)
+    uid = ''
+    if 'id' in os.environ:
+        uid = os.environ['id']
+    if os.environ.get('rabbit', False) == 'true':
+        try:
+            channel = connect_rabbit()
+            body = {'id': uid, 'type': 'metadata', 'file_path': path, 'data': '',
+                    'results': {'tool': 'ncapture', 'version': get_version()}}
+            send_rabbit_msg(body, channel)
+        except Exception as e:
+            print(str(e))
